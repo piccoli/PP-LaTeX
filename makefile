2642a053345f8056773e4253c972b27ef03d496a
Ricardo Piccoli <rfbpiccoli@gmail.com>
2012-05-02 12:26:16 -0300

    First public release

---
+++ makefile
@@ -0,0 +1,66 @@
+# Exemplo de arquivo Makefile para construção de um documento
+# LaTeX, utilizando a classe pucrs-ppgcc.cls.
+# por Ricardo Piccoli <rfbpiccoli at gmail.com>
+#
+# !!ATENÇÃO!! O uso incorreto deste script pode causar a perda ou
+# sobrescrita de arquivos no diretório onde estiver sendo
+# utilizado! Examine este arquivo com cuidado e, em caso de
+# dúvidas, NÃO o utilize para compilar o seu documento! Não me
+# responsabilizo pela perda acidental de arquivos!
+
+# Coloque em TARGET o nome (sem a extensão .tex) do seu documento
+# (apenas o documento principal). Como alternativa, coloque na
+# linha de comando:
+# make TARGET=exemplo
+TARGET=exemplo
+PDFDIR=pdf
+
+RM=rm -f
+#RM=rm -i # para evitar remoções indesejadas de arquivos.
+
+# Substitua por 'pdflatex', caso deseje incluir figuras .png ou
+# .jpg no seu documento.
+LATEX=latex
+#LATEX=pdflatex
+BIBTEX=bibtex
+SORT=./sort.sh
+
+# Verifique se sua instalação do LaTeX e Ghostscript
+# possuem estes comandos.
+PDFOPT=pdfopt
+ifeq ($(LATEX),latex)
+DVIPS=dvips
+DVIPSOPTS=-P pdf -t a4 -Pdownload35
+PS2PDF=ps2pdf -dPDFSETTINGS=/prepress
+endif
+
+all: $(TARGET).pdf
+	mkdir -p $(PDFDIR)
+	$(PDFOPT) $< $(PDFDIR)/$<
+
+ifeq ($(LATEX),pdflatex)
+$(TARGET).pdf: $(TARGET).tex
+else
+$(TARGET).pdf: $(TARGET).ps
+	$(PS2PDF) $<
+
+$(TARGET).ps: $(TARGET).dvi
+	$(DVIPS) $(DVIPSOPTS) -o $@ $<
+
+$(TARGET).dvi: $(TARGET).tex
+endif
+	$(LATEX) $<
+	$(BIBTEX) $(TARGET)
+	$(LATEX) $<
+	# comente a próxima linha se estiver em um ambiente Windows,
+	# sem o Cygwin ou ambientes similares.
+	$(SORT) $(TARGET)
+	$(LATEX) $<
+
+# Cuidado! 'make clean' irá apagar quaisquer arquivos .pdf no
+# diretório atual. Retire a entrada '*.pdf' abaixo se não desejar
+# este comportamento.
+clean:
+	$(RM) *.aux *.log *.bbl *.blg *.out *.toc *.loa *.lob *.lof *.los *.lot *.dvi *.ps *.pdf
+# Essa sintaxe aparentemente só funciona no bash:
+#	$(RM) *.{aux,log,bbl,blg,out,toc,loa,lob,lof,los,lot,dvi,ps,pdf}
---
+++ sort.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+##
+## Copyright (C) 2012 Ricardo Piccoli <rfbpiccoli at gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+# sort.sh: ordena as listas de abreviaturas e siglas para o
+# formato LaTeX do modelo de teses, monografias e dissertações do
+# PPGCC/PUCRS (2009).
+#
+# Requer uma distribuição UNIX com os utilitários 'sort', 'mv' e
+# 'bash'. Estes são comumente encontrados na maioria das
+# distribuições UNIX, ou ainda, Cygwin e MinGW para sistemas
+# Windows.
+#
+# !!ATENÇÃO!! O uso incorreto deste script pode causar a perda ou
+# sobrescrita de arquivos no diretório onde estiver sendo
+# utilizado! Examine este arquivo com cuidado e, em caso de
+# dúvidas, NÃO o utilize para compilar o seu documento! Não me
+# responsabilizo pela perda acidental de arquivos!
+
+function abort() {
+   echo "$@" > /dev/stderr
+   exit 1
+}
+
+function check() {
+   command -v $1 >& /dev/null || abort "Utilitário '$1' não encontrado. Abortando..."
+}
+
+FILE="$1"
+CLS="pucrs-ppgcc.cls"
+
+if [[ $# -lt 1 ]]; then
+    abort "Sintaxe: $0 <documento.tex>."
+fi
+
+if [[ ! -f "${FILE}.loa" ]] ||\
+   [[ ! -f "${FILE}.lob" ]]; then
+# FIXME O usuário pode simplesmente não estar usando estas
+# listas, então deve-se sair silenciosamente.
+#   abort "Execute o LaTeX pelo menos uma vez sobre o documento '${FILE}', utilizando a classe '$CLS'."
+   exit 0
+fi
+
+check mv
+check sort
+
+for i in a b; do
+    LFILE="${FILE}.lo${i}"
+    sort -d -f -o "${LFILE}~" "$LFILE" &&\
+    mv -f "${LFILE}~" "$LFILE"
+done
+
+echo "'$0 $@' concluído." > /dev/stderr
+exit 0
